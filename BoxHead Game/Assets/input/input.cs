20804309cf7d51b69cd426bdb7b37fb700916642
JacobVJ <jacobjensengg@gmail.com>
2023-01-11 12:54:15 +0100

    player control

--- BoxHead Game/Assets/input/input.cs
+++ BoxHead Game/Assets/input/input.cs
@@ -6,6 +6,8 @@ using UnityEngine.InputSystem;
 public class input : MonoBehaviour
 {
     private Rigidbody rb;
+    Vector3 movemenVector;
+    public float speed = 3;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +15,16 @@ public class input : MonoBehaviour
     }
     void OnMove(InputValue movementvalue)
     {
-        Vector2 movemenVector = movementvalue.Get<Vector2>();
-        transform.position = transform.position + new Vector3(movemenVector.x,0,movemenVector.y);
+        movemenVector = new Vector3(movementvalue.Get<Vector2>().x, 0, movementvalue.Get<Vector2>().y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = transform.position + movemenVector*Time.deltaTime*speed;
+        if(movemenVector != Vector3.zero)
+        {
+            transform.forward = movemenVector;
+        }
     }
 }
