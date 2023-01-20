5218a4a3418969e3d4f50da398903cb1b55acb82
JacobVJ <jacobjensengg@gmail.com>
2023-01-20 08:32:35 +0100

    20-01

--- BoxHead Game/Assets/spawnscript.cs
+++ BoxHead Game/Assets/spawnscript.cs
@@ -8,11 +8,13 @@ public class spawnscript : MonoBehaviour
     GameObject newEnemy;
     List<GameObject> zombie = new List<GameObject>();
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        for(int i = 0; i < 10; i++)
+        
+        for (int i = 0; i < 10; i++)
         {
-            newEnemy = Instantiate(enemy);
+            yield return new WaitForSeconds(3);
+            newEnemy = Instantiate(enemy,transform.position+new Vector3(Random.insideUnitCircle.x*20,0, Random.insideUnitCircle.y*20), Quaternion.identity);
             zombie.Add(newEnemy);
         } 
 
@@ -24,7 +26,7 @@ public class spawnscript : MonoBehaviour
         for (int i = 0; i < 10; i++)
         {
 
-            zombie[i].transform.position = Vector3.MoveTowards(zombie[i].transform.position, transform.GetChild(0).position, Time.deltaTime * 1);
+            zombie[i].transform.position = Vector3.MoveTowards(zombie[i].transform.position, transform.GetChild(0).position+new Vector3(Random.insideUnitCircle.x * 3, 0, Random.insideUnitCircle.y * 10), Time.deltaTime * 3);
         }
     }
 }
