5d6ab9955d41c990b68f144cdcf1216e92385f02
JacobVJ <jacobjensengg@gmail.com>
2023-02-03 08:37:14 +0100

    PickUpBox

--- BoxHead Game/Assets/Scripts/ZombiScipt.cs
+++ BoxHead Game/Assets/Scripts/ZombiScipt.cs
@@ -6,11 +6,19 @@ public class ZombiScipt : MonoBehaviour
 {
     int health = 500;
     public bool hunt=false;
+    public GameObject PickUpBox;
+
     public void TakeDamage(int Skade)
     {
         health = health - Skade;
         if(health <= 0)
-        {   Destroy(gameObject);    }
+        {
+            if (Random.Range(1, 2)==1)
+            {
+                PickUpBox = Instantiate(PickUpBox, transform.position,Quaternion.identity);
+            }
+            Destroy(gameObject);    
+        }
     }
 
     void Update()
--- BoxHead Game/Assets/input/input.cs
+++ BoxHead Game/Assets/input/input.cs
@@ -9,8 +9,10 @@ public class input : MonoBehaviour
     Vector3 movemenVector;
     public float speed = 5;
     public spawnscript spawnscript;
+    LayerMask Target;
     void Start()
     {
+        Target = LayerMask.GetMask("Zombi");
         rb = GetComponent<Rigidbody>();
     }
     void OnMove(InputValue movementvalue)
@@ -29,24 +31,24 @@ public class input : MonoBehaviour
         if(movemenVector != Vector3.zero)
         {
             transform.forward = movemenVector;
-        }      
+        }
     }
-
-
 
 
     int[] VåbenSkade = new int[] {72, 32, 156, 400 };
     string[] VåbenNavn = new string[] {"pistol","UZI", "Shotgun", "Sniper"};
    
+
+
     void ShootingRayCast()
     {
         Vector3 PlayerPosition = transform.position;
         Vector3 PlayerForward = transform.forward;
         Ray interactRay = new Ray(PlayerPosition, PlayerForward);
         RaycastHit interactHit;
-        float RayLength=5.0f;
+        float RayLength=10.0f;
         Vector3 EndPoint = PlayerForward * RayLength;
-        bool HitFound = Physics.Raycast(interactRay, out interactHit, RayLength);
+        bool HitFound = Physics.Raycast(interactRay, out interactHit, RayLength, Target);
         if (HitFound)
         {
             GameObject enemy = interactHit.transform.gameObject;
